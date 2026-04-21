4b95ba58d33e0439d4619909b016a2f11267f2f7
Tejas <tejaskarambe.tk@gmail.com>
2026-04-21 16:50:28 +0530

    fix updated login

--- controllers/authController.js
+++ controllers/authController.js
@@ -33,3 +33,27 @@ exports.resetPassword = (req, res) => {
     res.json({ message: "Password updated" });
   });
 };
+
+exports.changeOwnPassword = (req, res) => {
+  const { email, oldPassword, newPassword } = req.body;
+
+  authModel.login(email, (err, results) => {
+    if (err) return res.status(500).json(err);
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const user = results[0];
+
+    if (user.password !== oldPassword) {
+      return res.status(401).json({ message: "Old password incorrect" });
+    }
+
+    authModel.resetPassword(user.id, newPassword, (err) => {
+      if (err) return res.status(500).json(err);
+
+      res.json({ message: "Password updated successfully" });
+    });
+  });
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const controller = require("../controllers/authController");
 
 router.post("/login", controller.login);
 router.post("/reset-password", controller.resetPassword);
+router.post("/change-password", controller.changeOwnPassword);
 
 module.exports = router;
